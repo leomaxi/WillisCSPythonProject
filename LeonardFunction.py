1975431387caa86a77c6efc14555a0d37596a97c
Leonard M.I Mensah <leomaxi@outlook.com>
2025-04-15 23:36:50 -0230

    Count letter case in string

---
+++ LeonardFunction.py
@@ -0,0 +1,20 @@
+#Define function to count uppercase and lowercase letters in a string
+def letterCaseCounter(text):
+    # Initialize counters
+    uppercase_count = 0
+    lowercase_count = 0
+
+# loop through each character in the string
+    for char in text:
+        if char.isupper():
+            uppercase_count += 1
+        elif char.islower():
+            lowercase_count += 1
+
+    print("Total Uppercase Letters:", uppercase_count)
+    print("Total Lowercase Letters:", lowercase_count)
+
+# Get input from user
+user_input = input("Enter a string or sentence: ")
+# Call the function with user input
+letterCaseCounter(user_input)
